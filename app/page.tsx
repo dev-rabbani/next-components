cc98f9b0040fb6a39abf2a3a68cd22c681a8d9d4
dev-rabbani <rabbani.cse.69@gmail.com>
2023-07-18 00:30:43 +0600

    add sanity like header structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { NavigationMenuDemo } from "@/src/sections/header";
 import { ShopifySectionOne } from "@/src/sections/inspiration/shopify-section-one";
 
 export default function Home() {
   return (
     <div>
-      <ShopifySectionOne />
+      <NavigationMenuDemo />
+      {/* <ShopifySectionOne /> */}
     </div>
   );
 }
